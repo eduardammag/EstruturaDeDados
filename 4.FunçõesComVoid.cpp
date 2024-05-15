51dc401b57fdbc4a39e40c97b9b363c9806877a5
Eduarda Mesquita <126674559+eduardammag@users.noreply.github.com>
2024-05-14 21:14:38 -0300

    Add files via upload

---
+++ 4.FunçõesComVoid.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+// função antes da main ou assinatura/protótipo da função antes e definição depois da main
+// void é um vazio da linguagem
+
+
+void FrasePreferida()
+{
+    
+// a função não tem parâmetro, nem retorno, ela é vazia 
+// sai a frase no console, mas nada é guardado na memória
+    
+    cout << "Eu sou estudante de Ciência de Dados da FGV!" << endl;
+}
+
+
+void MontaFrase(char chArtigo, string strPalavra)
+{
+    
+//o retorno é vazio    
+    
+    cout << "Eu admiro " << chArtigo << " " << strPalavra << "!" << endl;
+}
+
+void MontaFrasePadrão(char chArtigo = 'a', string strPalavra= "Eduarda Mesquita")
+{
+     cout << "Eu admiro " << chArtigo << " " << strPalavra << "!" << endl;
+}
+
+int potenciacao(int iBase, int iExpoente)
+{
+    return pow(iBase,iExpoente); 
+    //pow= potenciacao
+}
+
+
+float modulo(float iValor)
+{
+    return fabs(iValor);
+    //abs:  função para tirar módulo de int
+    //fabs: função para tirar módulo de float
+}
+
+
+//Função para imprimir os elementos de um vetor de inteiros
+void ImprimindoElementos(int arriNumeros[], int iTamanhoVetor)
+{
+  //Itera sobre o vetor e imprime cada elemento
+  
+  //for(condição de incialização do loop; condição de continuação do loop(enquanto for verdadeira, o loop continuará); operador de incremento)
+    for (int i=0; i < iTamanhoVetor; i++)
+    {
+        cout << arriNumeros[i] << endl; 
+        //Imprime o elemento atual seguido de uma quebra de linha
+    }
+    
+}
+
+int main()
+{
+    
+    FrasePreferida();
+    MontaFrase('a', "Fernanda Vasconcelos");
+    // '' p/ caracteres e " " para string
+    
+    //MontaFrase("a", 'Fernanda Vasconcelos'); ERRO
+    //MontaFrase("a", "Fernanda Vasconcelos"); ERRO
+    //MontaFrase('a', 'Fernanda Vasconcelos'); ERRO
+    
+    MontaFrasePadrão();
+    MontaFrasePadrão('a', "minha mãe");
+    MontaFrasePadrão('a', "Shakira");
+    MontaFrasePadrão('a'); // funciona com um só argumento, é um argumento de posição (?)
+    // MontaFrasePadrão("Paula Eduarda"); ERRO
+    MontaFrasePadrão('a', "Mariana Fernandes");
+    
+    
+    potenciacao(2,1); //calcula, mas não imprime, precisa do cout
+    cout << "2 elevado a 3 é: " << potenciacao(2,3) << endl;
+    cout << "5 elevado a 6 é: " << potenciacao(5,6) << endl;
+
+    
+    cout << "O módulo de 13 é: " << modulo(13) << endl;
+    cout << "O módulo de -13 é: " << modulo(-13) << endl;
+    cout << "O módulo de 13.7 é: " << modulo(13.7) << endl;
+    cout << "O módulo de -13.7 é: " << modulo(13.7) << endl;
+
+
+    int arriVetorTeste[7] = {1,7,13,42,56,678,987};
+    int iTamanhoVetorTeste = sizeof(arriVetorTeste)/sizeof(arriVetorTeste[0]);
+    ImprimindoElementos(arriVetorTeste, iTamanhoVetorTeste);
+}
+
+
+
+
